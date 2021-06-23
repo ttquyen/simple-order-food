adaeb1fd4536fdac82d3f7952e06aa8c1638fafd
ttquyen <thuyquyentruong543@gmail.com>
2021-06-23 17:10:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Layout/Header'
+import React, { Fragment } from 'react'
+import Meals from './components/Meals/Meals';
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
